5f07e69e47bd9aba1581f28792921afc7a701ae0
tdakkota <tanc13@yandex.ru>
2023-12-26 10:00:26 +0300

    fix: do not try to set task cred for privileged containers

--- cmd/portoshim/runtime_mapper.go
+++ cmd/portoshim/runtime_mapper.go
@@ -179,9 +179,6 @@ func prepareCapabilities[C securitContextDesc](spec *pb.TContainerSpec, cfg C) {
 		spec.OwnerCred = &pb.TCred{
 			User: proto.String("root"),
 		}
-		spec.TaskCred = &pb.TCred{
-			User: proto.String("root"),
-		}
 		spec.VirtMode = proto.String("docker")
 	}
 }
